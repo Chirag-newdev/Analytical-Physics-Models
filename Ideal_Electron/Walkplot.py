1d927f0049922d4c09c573cb2137e4c9390a3c81
satkarjuneja <satkarjuneja@gmail.com>
2026-03-20 23:21:57 +0530

    Added model browser UI with iframe-based Plotly viewer

--- Ideal_Electron/Walkplot.py
+++ Ideal_Electron/Walkplot.py
@@ -1,29 +1,82 @@
 import numpy as np
 import matplotlib.pyplot as plt
 import random
+import plotly.graph_objects as go
 
-location_data = [0]
-step_data = [0]
-position = 0
-Num = 1000
 
-plt.ion()
-fig, ax = plt.subplots()
-plt.title("Drunkard")
-plt.xlabel("Steps")
-plt.ylabel("Location")
-plt.grid(True)
+def generate_walk(Num=1000):
+    position = 0
+    step_data = [0]
+    location_data = [0]
 
-for i in range(0, 1000):
-    step = random.choice([1, -1])
-    position += step
-    step_data.append(i)
-    location_data.append(position)
-    plt.cla()
-    plt.plot(step_data, location_data, color='black', linestyle='-')
-    plt.plot(i, position, 'o', color='red')
+    for i in range(1, Num + 1):
+        step = random.choice([1, -1])
+        position += step
+        step_data.append(i)
+        location_data.append(position)
+
+    return step_data, location_data
+
+
+# ------------------ MATPLOTLIB VERSION ------------------
+def run_matplotlib():
+    step_data = [0]
+    location_data = [0]
+    position = 0
+
+    plt.ion()
+    fig, ax = plt.subplots()
+    plt.title("Drunkard (Matplotlib)")
+    plt.xlabel("Steps")
+    plt.ylabel("Location")
     plt.grid(True)
-    plt.pause(0.05)
 
-plt.ioff()
-plt.show()
+    for i in range(1, 1000):
+        step = random.choice([1, -1])
+        position += step
+        step_data.append(i)
+        location_data.append(position)
+
+        plt.cla()
+        plt.plot(step_data, location_data, color='black', linestyle='-')
+        plt.plot(i, position, 'o', color='red')
+        plt.grid(True)
+        plt.pause(0.01)
+
+    plt.ioff()
+    plt.show()
+
+
+# ------------------ PLOTLY VERSION ------------------
+def run_plotly():
+    step_data, location_data = generate_walk(1000)
+
+    fig = go.Figure()
+
+    # full path
+    fig.add_trace(go.Scatter(
+        x=step_data,
+        y=location_data,
+        mode='lines',
+        name='Walk'
+    ))
+
+    # final position
+    fig.add_trace(go.Scatter(
+        x=[step_data[-1]],
+        y=[location_data[-1]],
+        mode='markers',
+        name='Final Position'
+    ))
+
+    fig.update_layout(
+        title="1D Random Walk of a Drunkard",
+        xaxis_title="Steps",
+        yaxis_title="Location"
+    )
+
+    fig.write_html("web_src/Walkplot.html")
+    fig.show()
+
+# run_matplotlib()
+run_plotly()
--- Ideal_Electron/means.py
+++ Ideal_Electron/means.py
@@ -1,50 +1,77 @@
-import matplotlib.pyplot as plt
 import numpy as np
-import random
-steps=500000
-electrons=100000
-
-positions=np.zeros(electrons)
-mean=[]
-meansq=[]
-
-fig,ax =plt.subplots(1,3,figsize=(18,6))
-
-walk_line, = ax[0].plot([], [], 'k-', label='x(t) for one electron')
-mean_line, = ax[1].plot([], [], 'b-', label='⟨x⟩')
-sq_line, = ax[2].plot([], [], 'r-', label='⟨x²⟩')
-
-for a in ax:
-    a.grid(True)
-    a.legend()
-    a.set_xlabel('Step')
-
-ax[0].set_ylabel('Position')
-ax[1].set_ylabel('⟨x⟩')
-ax[2].set_ylabel('⟨x²⟩')
-ax[0].set_title('Sample Electron Walk')
-ax[1].set_title('Mean ⟨x⟩')
-ax[2].set_title('Mean Squared ⟨x²⟩')
-
-x_sample=[]
-for i in range(0,steps):
-    num=np.random.choice([1,-1], size=electrons)
-    positions+=num
-
-    mean.append(np.mean(positions))
-    meansq.append(np.mean(positions**2))
-    x_sample.append(positions[0]) 
+import plotly.graph_objects as go
+from plotly.subplots import make_subplots
 
 
-    if i %500 == 0 or i == steps - 1:
-        walk_line.set_data(range(len(x_sample)), x_sample)
-        mean_line.set_data(range(len(mean)), mean)
-        sq_line.set_data(range(len(meansq)), meansq)
+def generate_walk(steps, electrons):
+    positions = np.zeros(electrons)
+    mean = []
+    meansq = []
+    x_sample = []
 
-        for a in ax:
-            a.relim()
-            a.autoscale_view()
-        plt.pause(0.05)
-plt.show()
+    for i in range(steps):
+        num = np.random.choice([1, -1], size=electrons)
+        positions += num
+
+        mean.append(np.mean(positions))
+        meansq.append(np.mean(positions**2))
+        x_sample.append(positions[0])
+
+    return mean, meansq, x_sample
 
 
+def run_plotly(steps=50000, electrons=10000):
+    mean, meansq, x_sample = generate_walk(steps, electrons)
+
+    fig = make_subplots(
+        rows=1, cols=3,
+        subplot_titles=("Sample Electron Walk", "Mean ⟨x⟩", "Mean ⟨x²⟩")
+    )
+
+    # x-axis (step index)
+    steps_axis = list(range(len(x_sample)))
+
+    # 1st plot
+    fig.add_trace(
+        go.Scatter(x=steps_axis,
+                   y=x_sample,
+                   mode='lines'),
+        row=1, col=1
+    )
+
+    # 2nd plot
+    fig.add_trace(
+        go.Scatter(x=steps_axis,
+                   y=mean, mode='lines',
+                   name='⟨x⟩'),
+        row=1, col=2
+    )
+
+    # 3rd plot
+    fig.add_trace(
+    go.Scatter(
+        x=steps_axis,
+        y=meansq,
+        mode='lines',
+        name='⟨x²⟩',
+        line=dict(color='red')
+    ),
+    row=1, col=3
+)
+
+    fig.update_xaxes(title_text="Step", row=1, col=1)
+    fig.update_xaxes(title_text="Step", row=1, col=2)
+    fig.update_xaxes(title_text="Step", row=1, col=3)
+
+    fig.update_yaxes(title_text="Position", row=1, col=1)
+    fig.update_yaxes(title_text="⟨x⟩", row=1, col=2)
+    fig.update_yaxes(title_text="⟨x²⟩", row=1, col=3)
+
+    fig.update_layout(title="Electron Random Walk Statistics")
+
+    fig.write_html("web_src/means.html")
+    fig.show()
+
+
+# if __name__ == "__main__":
+run_plotly()
