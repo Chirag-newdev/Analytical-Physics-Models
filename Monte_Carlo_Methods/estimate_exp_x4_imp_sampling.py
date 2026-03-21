fd64fdd1406bbd161478ef3861fcecee08437637
satkarjuneja <satkarjuneja@gmail.com>
2026-03-22 00:27:33 +0530

    Add more models

---
+++ Monte_Carlo_Methods/estimate_exp_x4_imp_sampling.py
@@ -0,0 +1,11 @@
+#We have to estimate the value of Expectation(X**2) where X is sampled from exp(-x^4)/Z 
+# where Z is just some normalization constant
+#lets choose simple e^(-x^4+1/2) for importance sampling as it matches with the maxima of our function
+
+import numpy as np
+import matplotlib.pyplot as plt
+e=np.e
+
+def generate(N):
+    pass
+    
