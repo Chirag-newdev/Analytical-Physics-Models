7d4bb764dcd0646c5afcc4fbd16b86e74e631609
satkarjuneja <satkarjuneja@gmail.com>
2026-03-22 07:02:31 +0530

    deploy fullstack app with active generation of paths in UI

--- backend.py
+++ backend.py
@@ -26,4 +26,4 @@ app.mount("/", StaticFiles(directory=".",html=True), name="static")
 
 
 if __name__ == "__main__":
-    uvicorn.run(app,host="0.0.0.0",port=8000)
+    uvicorn.run(app,host="0.0.0.0",port=7860)
